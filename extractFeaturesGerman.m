c88d8f32819450a26e0098e9c87c1da432cb8f31
Larry Clarfeld <lclarfel@uvm.edu>
2016-11-07 21:36:11 -0500

    Added field to featuresALL variable for gender

--- extractFeaturesGerman.m
+++ extractFeaturesGerman.m
@@ -1,6 +1,8 @@
 win = 0.02; % Window/frame size at 20 ms
 step = win - win/4; % Overlap by 25%
 
+male = [3 10 11 12 15];
+
 dirpath = 'archive';
 listing = dir(dirpath);
 ct = 0;
@@ -17,6 +19,12 @@ for i = 1:length(listing)
         featuresALL(ct).speaker = str2double(filename(1:2));
         featuresALL(ct).textcode = filename(3:5);
         featuresALL(ct).emotion = filename(6);
+        
+        if sum(ismember(male,featuresALL(ct).speaker))
+            featuresALL(ct).gender = 'm';
+        else
+            featuresALL(ct).gender = 'f';
+        end
 
         if filename(7) ~= '.'
             featuresALL(ct).vers = filename(7);
@@ -25,3 +33,5 @@ for i = 1:length(listing)
         end
     end
 end
+
+save('featuresGerman.mat','featuresALL')
