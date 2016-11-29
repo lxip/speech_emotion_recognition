64174fd4df3c8e2672b9b4ad3bc94910e2bae426
xipei <lianaliu@outlook.com>
2016-11-29 07:34:09 -0500

    tials with KNN

--- KNNtestDrive.m
+++ KNNtestDrive.m
@@ -1,58 +1,95 @@
 % A drive for running KNN classifier in the library.
+% And plot for showing the functionality on selected emotions.
 % Creator: Xipei Liu
 % Last Edited: 2016-11-18
 clc;
 clear all;
 load('featuresGerman.mat')
 
-
-% emos = unique([featuresALL.emotion])'; % All emotions
-% speakers = unique([featuresALL.speaker])'; % All speakers
+% iter = [1,5,10,30,50,100,200,300,500,1000];
 % 
-% emo12 = datasample(emos',2,'Replace',false);
-
-
-% Pick out 2 emotions
-emo1 = 'W'; %anger
-emo2 = 'F'; %happiness
-
-% select out just those 2 emotions
+% for loop = 1:6
+emos = unique([featuresALL.emotion])'; % All unique emotions
+speakers = unique([featuresALL.speaker])'; % All unique speakers
 emotions = [featuresALL.emotion]';
+numfeatures = size([featuresALL.features],1); 
+numstats = 5; % mean,median,std,min,max
 
-data_emo1 = featuresALL(emotions == emo1);
-data_emo2 = featuresALL(emotions == emo2);
+emoPick = datasample(emos',2,'Replace',false); 
+disp(['Emotion ',emoPick(1), ' and ',emoPick(2),' are picked.']);
 
-%%
-% Set up the data matrix
-numfeatures = 35;
-numstats = 5;
-featureData_emo1 = zeros(length(data_emo1), numfeatures*numstats);
-featureData_emo2 = zeros(length(data_emo2), numfeatures*numstats);
-%%
-
-for i = 1:size(featureData_emo1,1)
-    featureData_emo1(i,(1:35)*numstats-(numstats-1)) = mean  (data_emo1(i).features, 2);
-    featureData_emo1(i,(1:35)*numstats-(numstats-2)) = median(data_emo1(i).features, 2);
-    featureData_emo1(i,(1:35)*numstats-(numstats-3)) = std   (data_emo1(i).features,0,2);
-    featureData_emo1(i,(1:35)*numstats-(numstats-4)) = min   (data_emo1(i).features,[],2);
-    featureData_emo1(i,(1:35)*numstats-(numstats-5)) = max   (data_emo1(i).features,[],2); 
+for i = 1:length(emoPick)
+    feature_emo{i} = featuresALL(emotions==emoPick(i));
+    featureData{i} = zeros(numfeatures*numstats, length(feature_emo{i}));
 end
 
-for j = 1:size(featureData_emo2,1)
-    featureData_emo2(j,(1:35)*numstats-(numstats-1)) = mean  (data_emo2(j).features, 2);
-    featureData_emo2(j,(1:35)*numstats-(numstats-2)) = median(data_emo2(j).features, 2);
-    featureData_emo2(j,(1:35)*numstats-(numstats-3)) = std   (data_emo2(j).features,0,2);
-    featureData_emo2(j,(1:35)*numstats-(numstats-4)) = min   (data_emo2(j).features,[],2);
-    featureData_emo2(j,(1:35)*numstats-(numstats-5)) = max   (data_emo2(j).features,[],2); 
+for i = 1:length(featureData)
+   for j = 1:size(featureData{i},2)
+       featureData{i}((1:numfeatures)+numfeatures*(numstats-5),j) = mean  (feature_emo{i}(j).features,2)';
+       featureData{i}((1:numfeatures)+numfeatures*(numstats-4),j) = median(feature_emo{i}(j).features,2)';
+       featureData{i}((1:numfeatures)+numfeatures*(numstats-3),j) = std   (feature_emo{i}(j).features,0,2)';
+       featureData{i}((1:numfeatures)+numfeatures*(numstats-2),j) = min   (feature_emo{i}(j).features,[],2)';
+       featureData{i}((1:numfeatures)+numfeatures*(numstats-1),j) = max   (feature_emo{i}(j).features,[],2)';
+   end
 end
-
 clearvars i j;
 
-featureData = {featureData_emo1',featureData_emo2'};
+
+% %%% Experiment 1
+% % the performance for different reps: may speed up KNN(?)
+% 
+% accus_iter{loop} = zeros(2,length(iter));
+% for i = 1:length(iter)
+%     [CM1, Ac1, Pr1, Re1, F11, CM2, Ac2, Pr2, Re2, F12] = ...
+%         evaluateClassifier(featureData, 2, 1, [0.8,iter(i)]);
+%     accus_iter{loop}(1,i) = Ac1;
+%     accus_iter{loop}(2,i) = Ac2;
+% end
+% end
+% 
+% %%%
+% figure('units','normalized','position',[.1 .1 .8 .4])
+% hold on
+% for loop=1:6
+%     plot(iter,accus_iter{loop}(1,:)','-*')
+%     plot(iter,accus_iter{loop}(2,:)','-^')
+% end
+% set(gca,'XTick',iter)
+% set(gca,'xscale','log')
+% xlabel(['feature # for emotion ',emoPick(1),' and ',emoPick(2)])
+% ylabel('accuracy')
+% legend('Ac for G-normalization','Ac for RW-normalization')
+% title('KNN: different iteration for choosen 6 emotions classification result (shown in different colors)')
 
 
+%% Experiment 2
+% the classification result for each feature, KNN working or not
+nreps = 2;
+accus1 = zeros(nreps, numfeatures*numstats);
+accus2 = zeros(nreps, numfeatures*numstats);
+for n = 1:nreps
+    for j = 1:numfeatures*numstats
+        for i = 1:length(featureData)
+            featureSelect{i} = zeros(numfeatures*numstats, length(feature_emo{i}));
+            featureSelect{i}(j,:) = featureData{i}(j,:);
+        end
+        [CM1, Ac1, Pr1, Re1, F11, CM2, Ac2, Pr2, Re2, F12] = ...
+                evaluateClassifier(featureSelect, 2, 1, [0.8,100]);
+        % repeat 100 times for each classification, 80%train?ing 20%testing
+        accus1(n,j) = Ac1;
+        accus2(n,j) = Ac2;
+    end
+end
+%%%
+figure('units','normalized','position',[.1 .1 .8 .4])
+hold on
+plot(mean(accus1),'-*')
+plot(mean(accus2),'-^')
+set(gca,'XTick',linspace(0,175,6))
+xlabel(['feature # for emotion ',emoPick(1),' and ',emoPick(2)])
+ylabel('accuracy')
+legend('Ac for G-normalization','Ac for RW-normalization')
+title('KNN: single feature classification result for selected emotions')
+      
 
-%%
-[CM1, Ac1, Pr1, Re1, F11, CM2, Ac2, Pr2, Re2, F12] = ...
-                evaluateClassifier(featureData, 2, 1, [0.8,500]);
-            
+
